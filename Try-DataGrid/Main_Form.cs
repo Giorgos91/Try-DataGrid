402cdda992a14e23fa9746258ac03dce656f47b7
Giorgos91 <giorgos18hadjimichael@gmail.com>
2021-11-16 19:41:09 +0200

    Add project files.

---
+++ Try-DataGrid/Main_Form.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Try_DataGrid
+{
+    public partial class mainForm : Form
+    {
+
+        public List<Person> people = new List<Person>();
+        public mainForm()
+        {
+            Add();
+            InitializeComponent();
+        }
+
+        public void Add()
+        {
+            people.Add(new Person("Marios", "Kosta", 1926, 22, 1.83));
+            people.Add(new Person("Andreas", "Perdikou", 3695, 23, 1.90));
+            people.Add(new Person("Giorgos", "Michael", 1924, 25, 1.85));
+        }
+        private void mainForm_Load(object sender, EventArgs e)
+        {
+            var people = this.people;
+
+            dataGridView1.DataSource = people;
+        }
+    }
+}
---
+++ Try-DataGrid/Person.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Try_DataGrid
+{
+    public class Person
+    {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public int Id { get; set; }
+        public int Age { get; set; }
+        public double Height { get; set; }
+
+
+        public Person(string name, string surname,int id,int age,double height)
+        {
+            Name = name;
+            Surname = surname;
+            Id = id;
+            Age = age;
+            Height = height;
+        }
+    }
+}
